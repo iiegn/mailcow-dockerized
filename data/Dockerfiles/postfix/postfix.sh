131578e95ed05f477b2fcc4dc0c4254a5d4a80d9
egon w. stemle <egon@iiegn.eu>
2018-10-16 12:24:00 +0200

    Revert "fixup! [Dovecot] Add sender_dependent_relayhost mapping for individual users"
    This reverts commit 55356a15a2faa24e90d1477c680a3f4a0acc6cd2.

--- data/Dockerfiles/postfix/postfix.sh
+++ data/Dockerfiles/postfix/postfix.sh
@@ -368,8 +368,6 @@ postfix set-permissions
 
 # Create local lookup tables
 postmap /opt/postfix/conf/check_sasl_access
-postmap /opt/postfix/conf/sender_dependent_relayhost_maps
-postmap /opt/postfix/conf/sasl_passwd
 
 # Check Postfix configuration
 postconf -c /opt/postfix/conf > /dev/null
