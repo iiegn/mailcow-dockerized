887e1b9bc8feab8043951eb32de3ba2d5c12556d
egon w. stemle <github@iiegn.de>
2024-05-26 01:20:51 +0200

    Merge branch 'localfix/default_mailer_aliases' -- data/Dockerfiles/postfix/postfix.sh

--- data/Dockerfiles/postfix/postfix.sh
+++ data/Dockerfiles/postfix/postfix.sh
@@ -410,11 +410,15 @@ user = ${DBUSER}
 password = ${DBPASS}
 hosts = unix:/var/run/mysqld/mysqld.sock
 dbname = ${DBNAME}
-query = SELECT \'${VIRTUAL_POSTMASTER_DEFAULT}\' AS goto FROM alias
-  WHERE (domain IN
+query = SELECT '${VIRTUAL_POSTMASTER_DEFAULT}' AS goto FROM alias
+  WHERE ((domain IN
       (SELECT domain FROM domain
         WHERE domain='%d'
           AND active='1'))
+     OR (domain in
+      (SELECT alias_domain FROM alias_domain
+        WHERE alias_domain='%d'
+          AND active='1')))
     AND ('%s' REGEXP '^(MAILER-DAEMON|postmaster|abuse|webmaster)@')
     LIMIT 1;
 EOF
